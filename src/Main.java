26ce4ceadb5ed90d167dd00eb8daa23a3befb1b4
Razib Hossen <razibhossendev@gmail.com>
2023-09-11 20:22:05 +0600

    java first project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+class sutdent
+{
+    public static void main (String agrs [])
+    {
+        if (5>6)
+        {
+            System.out.println("Good job");
+        }
+        else {
+            System.out.println("Program End");
+        }
+
+    }
+}
---
+++ src/Student1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Student1
+{
+  public static void main (String args [])
+  {
+      int a;
+      float b;
+      String str;
+      Scanner obj = new Scanner(System.in);
+      System.out.println("Enter String");
+      str = obj.nextLine();
+      System.out.println("string "+str);
+      System.out.println("Enter Integer");
+      a = obj.nextInt();
+      System.out.println("Integer "+a);
+      System.out.println("Enter Float ");
+      b = obj.nextFloat();
+      System.out.println("Float "+b);
+
+  }
+}
